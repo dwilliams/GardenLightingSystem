b64f21e0edc3b86265df5147c1e38725c3cf59b7
Daniel Williams <dwilliams@port8080.net>
2016-07-21 08:22:44 -0600

    Updates to get the function generator working.

--- firmware/GardenLightingSystem/FunctionGenerator.ino
+++ firmware/GardenLightingSystem/FunctionGenerator.ino
@@ -12,13 +12,24 @@ void FunctionGenerator_Setup() {
 
   // Setup the function generator for 400 Hz
   FunctionGenerator_fgen.reset();
-  delay( 100);
-  FunctionGenerator_fgen.setMode( MiniGen::SINE);
-  delay( 100);
-  uint32_t freqRegVal = FunctionGenerator_fgen.freqCalc( 400);
-  FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ0, MiniGen::FULL, freqRegVal);
-  delay( 100);
-  FunctionGenerator_fgen.selectFreqReg( MiniGen::FREQ0);
+
+  // NOTE: There's a bug that causes the command register to have the first bit set to '1' when
+  //          said bit should be 0.
+  //FunctionGenerator_fgen.setMode( MiniGen::SINE);
+  
+  uint16_t freqRegVal = FunctionGenerator_fgen.freqCalc( 400);
+  //FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ0, MiniGen::FULL, freqRegVal);
+  //FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ1, MiniGen::FULL, freqRegVal);
+  FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ0, freqRegVal);
+  FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ1, freqRegVal);
+  
+  FunctionGenerator_fgen.adjustPhaseShift( MiniGen::PHASE0, 0x0000);
+  FunctionGenerator_fgen.adjustPhaseShift( MiniGen::PHASE1, 0x0000);
+  
+  //SPIWrite(0x0100);
+  //SPIWrite(0x0000);
+  
+  //FunctionGenerator_fgen.selectFreqReg( MiniGen::FREQ0);
   
   Serial.print( "freqRegVal: ");
   Serial.print( freqRegVal, HEX);

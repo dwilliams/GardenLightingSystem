9c3ceaca00674456fe909b0ec5187dbd0713dcf1
Daniel Williams <dwilliams@port8080.net>
2016-07-23 12:19:16 -0600

    Decided against the SparkFun Minigen library as I just needed to send a few bytes to initialize the chip.  Now I'm just handling the SPI directly.

--- firmware/GardenLightingSystem/FunctionGenerator.ino
+++ firmware/GardenLightingSystem/FunctionGenerator.ino
@@ -1,37 +1,53 @@
 // Put the functions for the function generator here.
 
 #include <SPI.h>
-#include <SparkFun_MiniGen.h>
 
-MiniGen FunctionGenerator_fgen;
+// Configure the normal mode of operation for the config register
+const uint16_t configReg = 0x0200;
 
-void FunctionGenerator_Setup() {
+// Configure the frequency register value for 400 Hz (400.0 / 0.0596).
+// Note: Make sure the upper two bits are 0 as they will be used to set the register
+const uint32_t freqRegVal = 0x1A37;
+
+void writeData( int csPin, uint16_t data) {
+  // Set the chip select pin to low to enable writing to the chip.
+  digitalWrite( csPin, LOW);
+  
+  // Write the data.
+  SPI.transfer( (byte) (data>>8));
+  SPI.transfer( (byte) data);
+  
+  // Set the chip select pin to high to disable writing to the chip.
+  digitalWrite( csPin, HIGH);
+}
+
+void FunctionGenerator_Setup( int csPin) {
   Serial.println( "Setting up function generator");
-  
-  FunctionGenerator_fgen = MiniGen( 2); // Using D2 for the chip select
 
-  // Setup the function generator for 400 Hz
-  FunctionGenerator_fgen.reset();
+  // Calculate the register value for 400 Hz.
+  uint32_t freqRegVal = 400.0 / 0.0596;
 
-  // NOTE: There's a bug that causes the command register to have the first bit set to '1' when
-  //          said bit should be 0.
-  //FunctionGenerator_fgen.setMode( MiniGen::SINE);
+  // Setup the chip select as an output.
+  digitalWrite( csPin, HIGH);
+  pinMode( csPin, OUTPUT);
+
+  // Begin the SPI transaction with settings that are safe for the AD9837
+  SPI.beginTransaction( SPISettings( 10000000, MSBFIRST, SPI_MODE2));
+
+  // Reset the chip using it's internal reset function.
+  writeData( csPin, 0x0100);
+  writeData( csPin, configReg);
+
+  // Set the Frequency and Phase registers
+  writeData( csPin, ( 0x4000 | freqRegVal));  // Set Freq0 to 400 Hz.
+  writeData( csPin, ( 0x8000 | freqRegVal));  // Set Freq1 to 400 Hz.
+  writeData( csPin, 0xC000);  // Set Phase0 to 0.
+  writeData( csPin, 0xE000);  // Set Phase1 to 0.
+
+  // End the SPI transaction so other devices on the bus can be used.
+  SPI.endTransaction();
   
-  uint16_t freqRegVal = FunctionGenerator_fgen.freqCalc( 400);
-  //FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ0, MiniGen::FULL, freqRegVal);
-  //FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ1, MiniGen::FULL, freqRegVal);
-  FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ0, freqRegVal);
-  FunctionGenerator_fgen.adjustFreq( MiniGen::FREQ1, freqRegVal);
-  
-  FunctionGenerator_fgen.adjustPhaseShift( MiniGen::PHASE0, 0x0000);
-  FunctionGenerator_fgen.adjustPhaseShift( MiniGen::PHASE1, 0x0000);
-  
-  //SPIWrite(0x0100);
-  //SPIWrite(0x0000);
-  
-  //FunctionGenerator_fgen.selectFreqReg( MiniGen::FREQ0);
-  
-  Serial.print( "freqRegVal: ");
+  Serial.print( "Done setting up function generator using freqRegVal: ");
   Serial.print( freqRegVal, HEX);
   Serial.println( "");
 }
--- firmware/GardenLightingSystem/GardenLightingSystem.ino
+++ firmware/GardenLightingSystem/GardenLightingSystem.ino
@@ -1,13 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
 
+const int csPinFGen = 2;
+const int csPinDPot1 = 3;
+const int csPinDPot2 = 4;
+const int csPinDPot3 = 5;
+const int csPinDPot4 = 6;
+const int enPinAmp = 10;
+
+void setup() {
+  // Initialize the serial port to output debug info.
   Serial.begin( 9600);
 
-  FunctionGenerator_Setup();
+  // Initialize the SPI port since it'll be used all over the place.
+  SPI.begin();
 
+  // Setup the various functional groups.
+  FunctionGenerator_Setup( csPinFGen);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Do nothing for now.
 }
